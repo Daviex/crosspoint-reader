89d3a7f334e18e56e6add4493affc0ab4c713334
Daviex <david.iuffri94@hotmail.it>
2026-09-20 13:58:13 +0200

    style(test): align parser logging macro continuations

--- test/koreader_parser_lifetime/stubs/Logging.h
+++ test/koreader_parser_lifetime/stubs/Logging.h
@@ -3,9 +3,9 @@
 #include <cstdio>
 
 #define LOG_DBG(...) ((void)0)
-#define LOG_ERR(tag, ...)                    \
-  do {                                       \
-    std::fprintf(stderr, "[%s] ", tag);        \
-    std::fprintf(stderr, __VA_ARGS__);        \
-    std::fputc('\n', stderr);                 \
+#define LOG_ERR(tag, ...)               \
+  do {                                  \
+    std::fprintf(stderr, "[%s] ", tag); \
+    std::fprintf(stderr, __VA_ARGS__);  \
+    std::fputc('\n', stderr);           \
   } while (false)
